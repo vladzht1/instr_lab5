4214882a7209a596edb8e40351d47c5f7c8745c6
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-11-29 17:37:28 +0300

    add factorial function

--- Main.java
+++ Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
     }
+
+    private static int factorial(int num) {
+        if (num <= 0) {
+            return 1;
+        }
+
+        return num * (num - 1);
+    }
 }
