8b81f0ce4214eab011bd307a54d4adae57ceba9a
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-11-29 17:46:35 +0300

    another changes at main

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        System.out.print("Ещё изменения в main");
+
         System.out.println("Это добавлено в ветке dev");
 
         // 1
