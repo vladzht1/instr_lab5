51748b24291c948abfc330034403eb500393d7a7
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-11-29 17:39:21 +0300

    call factorial

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ import javax.sound.midi.Soundbank;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
+
+        // 1
+        System.out.println(factorial(1));
+
+        // 25
+        System.out.println(factorial(5));
     }
 
     private static int factorial(int num) {
